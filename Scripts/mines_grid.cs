bd5a03d3478c175bee1ee369e97dd7d6dcd47583
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-09-29 19:49:13 +0700

    Add for loop

--- Scripts/mines_grid.cs
+++ Scripts/mines_grid.cs
@@ -3,17 +3,25 @@ using System;
 
 public partial class mines_grid : TileMap
 {
-	public float columns = 8;
-	public float rows = 8;
-	public float number_of_mines = 8;
+	public int columns = 8;
+	public int rows = 8;
+	public int number_of_mines = 8;
 
-	var TILE_SET_ID = 0;
-	var DEFAULT_LAYER = 0;
+	int TILE_SET_ID = 0;
+	int DEFAULT_LAYER = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ClearLayer(DEFAULT_LAYER);
+
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				Console.WriteLine(i);
+			}
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
