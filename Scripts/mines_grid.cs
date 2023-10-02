a7a64625a403b56a8f93fad37109997021b1bdb8
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-02 20:46:31 +0700

    Add CELLS

--- Scripts/mines_grid.cs
+++ Scripts/mines_grid.cs
@@ -1,8 +1,28 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 public partial class mines_grid : TileMap
 {
+	int angkaX;
+	int angkaY;
+	Dictionary<string, string> CELLS = new Dictionary<string, string>() {
+		{"1", "Vector2I"},
+		{"2", "Vector2I"},
+		{"3", "Vector2I"},
+		{"4", "Vector2I"},
+		{"5", "Vector2I"},
+		{"6", "Vector2I"},
+		{"7", "Vector2I"},
+		{"8", "Vector2I"},
+		{"CLEAR", "Vector2I"},
+		{"MINE_RED", "Vector2I"},
+		{"FLAG", "Vector2I"},
+		{"MINE", "Vector2I"},
+		{"DEFAULT", "Vector2I"},
+	};
+
 	public int columns = 8;
 	public int rows = 8;
 	public int number_of_mines = 8;
